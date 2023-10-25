c71e7d633fe497c33ed5c9f23b62b0c8a31adc48
DOMINION JOHN <119686378+DOMINION-JOHN1@users.noreply.github.com>
2023-10-25 13:24:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ yes_no_mapping = {'No': 0, 'Yes': 1}
 
 # Streamlit app header
 st.title('Customer Churn Prediction')
-image=Image.open("https://raw.githubusercontent.com/DOMINION-JOHN1/bank-customer-churn-prediction/main/bank.jpg")
+image=Image.open("bank.jpg")
 st.image(image,use_column_width="always")
 
 
