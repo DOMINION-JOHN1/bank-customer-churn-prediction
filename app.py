0a4c1a3523741fcfe57e696028b8c8dc4c0f8351
DOMINION JOHN <119686378+DOMINION-JOHN1@users.noreply.github.com>
2023-10-30 22:19:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ if st.sidebar.button('Predict'):
     # Preprocess the user input (standardization)
     user_input = [credit_score, geography, gender, age, tenure, balance, num_of_products, has_cr_card, is_active_member, estimated_salary]
     scaler = StandardScaler()
-    user_input_scaled = scaler.fit_transform(user_input.reshape(1, -1))
+    user_input_scaled = scaler.fit_transform(np.array(user_input).reshape(1, -1))
 
     # Make predictions using the Random Forest Classifier model
     prediction = loaded_model.predict(user_input_scaled)
